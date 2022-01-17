2965610685c162efa265418d4c6b7722c6d552f3
junilpyeon <dreamerjunil@gmail.com>
2022-01-17 16:36:08 +0900

    국제화 적용

--- item-service/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ item-service/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,12 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +15,4 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-
 }
